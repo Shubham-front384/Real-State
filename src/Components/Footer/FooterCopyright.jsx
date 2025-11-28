329c26d73ca28ec1f32c28cf58e1f73d42bcecde
Shubham-front384 <shubhamworkemail07@gmail.com>
2025-11-28 11:16:57 +0530

    Home Section Done

--- src/Components/Footer/FooterCopyright.jsx
+++ src/Components/Footer/FooterCopyright.jsx
@@ -1,9 +1,22 @@
 
 const FooterCopyright = () => {
   return (
-    <>
-      <h5>FooterCopyright</h5>
-    </>
+    <div className="p-4 bg-[#1a1a1a] md:p-7">
+      <div className="flex flex-col items-center gap-8 py-8 md:flex-row md:justify-between">
+        <div className="logo flex gap-4 md:order-2">
+          <i className="ri-facebook-fill text-3xl p-4 px-5 rounded-full bg-gray-500"></i>
+          <i className="ri-linkedin-box-fill text-3xl p-4 px-5 rounded-full bg-gray-500"></i>
+          <i className="ri-twitter-fill text-3xl p-4 px-5 rounded-full bg-gray-500"></i>
+          <i className="ri-youtube-fill text-3xl p-4 px-5 rounded-full bg-gray-500"></i>
+        </div>
+        <div className="copyright">
+          <p className="text-center capitalize font-medium text-[14px] flex flex-col gap-2 md:order-1 md:flex-row md:gap-6 lg:text-[18px]">
+            @2025 estatein. all rights reserved.
+            <span>terms & conditions</span>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Footer/FooterMenu.jsx
+++ src/Components/Footer/FooterMenu.jsx
@@ -1,44 +1,44 @@
 
 const FooterMenu = () => {
   return (
-    <>
-      <div>
-        <ul className="home-links">
-          <li>Home</li>
-          <li>Hero Section</li>
-          <li>Features</li>
-          <li>Properties</li>
-          <li>Testimonials</li>
-          <li>FAQ's</li>
+    <div className="p-4 md:p-7">
+      <div className="py-4 md:flex md:gap-4 lg:gap-10" style={{columns: "120px"}}>
+        <ul className="home-links w-full flex flex-col gap-2 pb-4 border-b-2 border-gray-400 md:border-0">
+          <li className="font-medium text-lg text-gray-400 capitalize md:text-[18px] lg:text-[20px]">Home</li>
+          <li className="font-medium text-[14px] md:text-[16px] lg:text-[18px]">Hero Section</li>
+          <li className="font-medium text-[14px] md:text-[16px] lg:text-[18px]">Features</li>
+          <li className="font-medium text-[14px] md:text-[16px] lg:text-[18px]">Properties</li>
+          <li className="font-medium text-[14px] md:text-[16px] lg:text-[18px]">Testimonials</li>
+          <li className="font-medium text-[14px] md:text-[16px] lg:text-[18px]">FAQ's</li>
         </ul>
-        <ul className="about-links">
-          <li>about us</li>
-          <li>our story</li>
-          <li>our works</li>
-          <li>how it works</li>
-          <li>our team</li>
-          <li>our clients</li>
+        <ul className="about-links w-full flex flex-col gap-2 pb-4 border-b-2 border-gray-400 md:border-0">
+          <li className="font-medium text-lg text-gray-400 capitalize md:text-[18px] lg:text-[20px]">about us</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">our story</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">our works</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">how it works</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">our team</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">our clients</li>
         </ul>
-        <ul className="properties-link">
-          <li>properties</li>
-          <li>portfolio</li>
-          <li>categories</li>
+        <ul className="properties-link w-full flex flex-col gap-2 pb-4 border-b-2 border-gray-400 md:border-0">
+          <li className="font-medium text-lg text-gray-400 capitalize md:text-[18px] lg:text-[20px]">properties</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">portfolio</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">categories</li>
         </ul>
-        <ul className="services-link">
-          <li>services</li>
-          <li>valuation mastery</li>
-          <li>strategic marketing</li>
-          <li>negotiation wizardry</li>
-          <li>closing success</li>
-          <li>property management</li>
+        <ul className="services-link w-full flex flex-col gap-2 pb-4 border-b-2 border-gray-400 md:border-0">
+          <li className="font-medium text-lg text-gray-400 capitalize md:text-[18px] lg:text-[20px]">services</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">valuation mastery</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">strategic marketing</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">negotiation wizardry</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">closing success</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">property management</li>
         </ul>
-        <ul className="contact-link">
-          <li>contact us</li>
-          <li>contact form</li>
-          <li>our offices</li>
+        <ul className="contact-link w-full flex flex-col gap-2 pb-4 border-b-2 border-gray-400 md:border-0">
+          <li className="font-medium text-lg text-gray-400 capitalize md:text-[18px] lg:text-[20px]">contact us</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">contact form</li>
+          <li className="font-medium text-[14px] capitalize md:text-[16px] lg:text-[18px]">our offices</li>
         </ul>
       </div>
-    </>
+    </div>
   )
 }
 
